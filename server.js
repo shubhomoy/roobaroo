b929ce74953f11ec1b98f3e796ee7020a9312855
shubhomoy <shubhomoy.tubu@gmail.com>
2017-03-07 18:53:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var path = require('path')
+var fs = require('fs');
+var request = require('request');
+var ytdl = require('youtube-dl');
+var ffmpeg = require('fluent-ffmpeg');
+
+var app = express();
+
+app.use(express.static('output'))
+
+app.get('/', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'index.html'))
+});
+app.get('/top-artists', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
+
+const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=";
+const YOUTUBE_API_KEY = 'AIzaSyAdfnHdZ4bDpty7baQC9PVkdnSqyuSql28';
+
+app.get('/api/fetchsong', function(req, res) {
+	var mp4 = './video.mp4';
+	var outputFile = './output/' + req.query.name + '.mp3';
+	var stream = fs.createWriteStream(mp4);
+	request(encodeURI(YOUTUBE_SEARCH_URL + req.query.name + '&key=' + YOUTUBE_API_KEY), function(error, response, body) {
+		response = JSON.parse(body);
+		if(response.items.length > 0) {
+			var item = response.items[0];
+			console.log('Please wait...');
+			var url = encodeURI('https://youtube.com/watch?v=' + item.id.videoId);
+			ytdl.getInfo(url, function(err, info) {
+				var minFormat = null;
+				var minSize = Number.MAX_SAFE_INTEGER;
+				info.formats.forEach(function(format) {
+					if(format.filesize < minSize) {
+						minFormat = format;
+						minSize = minFormat.filesize;
+					}
+				});
+				console.log('File size of ' + (minFormat.filesize/(1024*1024)).toFixed(2) + ' mb will be downloaded');
+				var video = ytdl(url, ['--format=' + minFormat.format_id]);
+				console.log('Downloading...');
+				video.pipe(stream);
+				stream.on('finish', function() {
+					console.log('Download completed');
+					console.log('Converting...');
+					proc = new ffmpeg({
+						source: mp4
+					}).toFormat('mp3').on('end', function() {
+						console.log('Done!');
+						fs.unlink(mp4);
+						res.send('finish');
+					}).saveToFile(outputFile);
+				});
+
+			});
+		}
+	});	
+});
+
+app.listen(8080, function() {
+	console.log('Listening to port ' + 8080);
+});
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var path = require('path');
+var env = process.env.WEBPACK_ENV;
+
+var DEV = path.resolve(__dirname, 'dev');
+var OUTPUT = path.resolve(__dirname, 'output');
+
+var config = {
+	entry: DEV + '/index.jsx',
+	output: {
+		path: OUTPUT,
+		filename: 'index.js'
+	},
+	externals: {
+		'Config': JSON.stringify(require('./dev/config/config.json')),
+		'React': 'react'
+	},
+	node: {
+		fs: 'empty'
+	},
+	target: 'web',
+	plugins: [
+		new webpack.DefinePlugin({
+	    	'process.env.NODE_ENV': '"production"'
+	    }),
+		new webpack.DefinePlugin({
+			'process.env': {
+		    	'NODE_ENV': '"production"'
+		    }
+		})
+	],
+	module: {
+		loaders: [{
+			include: DEV,
+			loader: 'babel-loader'
+		}]
+	}
+};
+
+module.exports = config;
